3285dfc039ece6d6552118913054bdba67be40b1
Prayush Twayana <prayush.twayana@gmail.com>
2025-01-08 16:11:36 +0545

    plugin-undotree

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require("config")
 
--- print("---------------------")
--- print("welcome back goose")
--- print("let's get started with this programming sesh")
--- print("---------------------------------------------")
+print("---------------------")
+print("welcome back goose")
+print("let's get started with this programming sesh")
+print("---------------------------------------------")
 
-vim.cmd.colorscheme "rose-pine-moon"
+vim.cmd.colorscheme "rose-pine-dawn"
 -- vim.cmd.colorscheme "catppuccin"
 vim.g.netrw_bufsettings = 'noma nomod nobl nowrap ro'
 vim.o.cursorline = true
